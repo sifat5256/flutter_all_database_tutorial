5ea68e3238b9de500a2000b041fec4be1adbf42e
IQRAMUL HASAN SIFAT <140641867+sifat5256@users.noreply.github.com>
2025-02-28 10:56:18 +0600

    up 1

--- lib/local_database/sqlite/data_base_helper/db_helper1.dart
+++ lib/local_database/sqlite/data_base_helper/db_helper1.dart
@@ -1,57 +1,46 @@
-
 import 'package:path/path.dart';
-
 import 'package:sqflite/sqflite.dart';
 
-class DBHelper{
+class DBHelper {
+  static final DBHelper _instance = DBHelper._internal();
+  factory DBHelper() => _instance;
+  DBHelper._internal();
+
   static Database? _database;
 
   Future<Database> get database async {
-
-    if(_database!=null){
+    if (_database != null) {
       return _database!;
     }
-
-    else {
-       _database = await _initDB();
-
-    }
-
+    _database = await _initDB();
+    return _database!;
   }
 
-  Future <Database> _initDB() async{
-    String path = join(await getDatabasesPath(),'my_database.db');
+  Future<Database> _initDB() async {
+    String path = join(await getDatabasesPath(), 'my_database.db');
     return await openDatabase(
       path,
       version: 1,
-      onCreate: (db,version){
+      onCreate: (db, version) {
         return db.execute(
           '''
           CREATE TABLE users (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
-           name TEXT,
-           age INTEGER
+          name TEXT,
+          age INTEGER
           )
-          '''
+          ''',
         );
-      }
-
+      },
     );
-
-
-
-
   }
 
   Future<void> insertUser(String name, int age) async {
-    final db = await DBHelper().database;
+    final db = await database; // Corrected this line
     await db.insert(
       'users',
       {'name': name, 'age': age},
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
-
-
-
 }
