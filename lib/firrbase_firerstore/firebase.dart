1c48d618490be9bc2d5c374e949c6d902ab792f3
IQRAMUL HASAN SIFAT <140641867+sifat5256@users.noreply.github.com>
2025-03-05 14:25:57 +0600

    up 2

---
+++ lib/firrbase_firerstore/firebase.dart
@@ -0,0 +1,24 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void addUser(String name,int age){
+
+  FirebaseFirestore.instance.collection("users").add({
+    "name":name,
+    "age":age,
+    "createdAt":FieldValue.serverTimestamp()
+  }).then((onValue){
+    print("user Add");
+  }).catchError((onError){
+    print("error $onError");
+  });
+}
+
+void fetchUsers()async{
+
+  QuerySnapshot snapshot = await FirebaseFirestore.instance.collection("users").get();
+  for(var doc in snapshot.docs){
+    print(doc["name"]);
+  }
+}
